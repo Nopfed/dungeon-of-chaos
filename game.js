5a75d678ba119016e03ef48def18e5d22282447f
Jim Toth <jim@spooky.email>
2016-01-13 02:23:36 -0500

    Updated fight()

--- game.js
+++ game.js
@@ -2,6 +2,10 @@ function Hero(name, hp, atk) {
 	this.name = name;
 	this.hp = hp;
 	this.atk = atk;
+
+	this.formatName = function () {
+		return this.name + '(' + this.hp + ')';
+	};
 };
 
 function dieRoll(rolls, sides){
@@ -11,28 +15,49 @@ function dieRoll(rolls, sides){
 };
 
 function fight() {
+	// set up both fighters
 	var fighterA = new Hero("Buttz", 12, 5);
 	var fighterB = new Hero("Skele-Bro", 6, 2);
 
 	var count = 0;
 
+	// announce the fight
+	output(fighterA.formatName() + ' vs. ' + fighterB.formatName());
+
+	// fight to the death!
 	while (fighterA.hp > 0 && fighterB.hp > 0) {
+		var attackRollA, attackRollB;
+
 		// iterate the roundo
 		count++;
 
-		// fighterA attacks fighterB
-		fighterB.hp -= dieRoll(1, fighterA.atk);
-		output(fighterB.name + ': ' + fighterB.hp);
-		
-		// fighterB attacks fighterA
-		fighterA.hp -= dieRoll(1, fighterB.atk);
-		output(fighterA.name + ': ' + fighterA.hp);
+		// both fighters roll for their attacks
+		attackRollA = dieRoll(1, fighterA.atk);
+		attackRollB = dieRoll(1, fighterB.atk);
 
-		// evaluate resultzors
+		// output fighterA attack roll 
+		output(fighterA.name + ' attacks with a ' + attackRollA);
+
+		// subtract attack roll from fighterB hp
+		fighterB.hp = fighterB.hp - attackRollA;
+		output(fighterB.name + ' is at ' + fighterB.hp);
+
+		// check if fighterB is dead
+		if (fighterB.hp <= 0) {
+			output(fighterA.formatName() + " wins!");
+			continue;
+		}
+		
+		// output fighterB attack roll 
+		output(fighterB.name + ' attacks with a ' + attackRollB);
+		
+		// subtract attack roll from fighterA hp
+		fighterA.hp = fighterA.hp - attackRollB;
+		output(fighterA.name + ' is at ' + fighterA.hp);
+
+		// check if fighter A is dead
 		if (fighterA.hp <= 0) {
-			output(fighterB.name + " wins!");
-		} else if (fighterB.hp <= 0) {
-			output(fighterA.name + " wins!");
+			output(fighterB.formatName() + " wins!");
 		}
 	}
 };
