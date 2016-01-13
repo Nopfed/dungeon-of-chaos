ca77684abf73919403fc27f154b75bb6eb223c9e
Jim Toth <jim@spooky.email>
2016-01-13 08:18:33 -0500

    Added player and mob selection dropdowns

--- app/game.js
+++ app/game.js
@@ -8,17 +8,25 @@ function DungeonGame(options) {
 
 	// UI selectors
 	this.fightLogSelector = options.fightLogSelector || 'fight-log';
+
 	this.playerNameSelector = options.playerNameSelector || 'player-name';
 	this.playerHpSelector = options.playerHpSelector || 'player-hp';
+	this.playerSelectSelector = options.playerSelectSelector || 'player-select';
+
 	this.mobNameSelector = options.mobNameSelector || 'mob-name';
 	this.mobHpSelector = options.mobHpSelector || 'mob-hp';
+	this.mobSelectSelector = options.mobSelectSelector || 'mob-select';
 
 	// Cached UI Element wrappers
 	this.fightLog = document.getElementById(this.fightLogSelector);
+
 	this.playerNameElem = document.getElementById(this.playerNameSelector);
 	this.playerHpElem = document.getElementById(this.playerHpSelector);
+	this.playerSelectElem = document.getElementById(this.playerSelectSelector);
+
 	this.mobNameElem = document.getElementById(this.mobNameSelector);
 	this.mobHpElem = document.getElementById(this.mobHpSelector);
+	this.mobSelectElem = document.getElementById(this.mobSelectSelector);
 
 	this.reset = function () {
 		// clear the log
@@ -26,8 +34,8 @@ function DungeonGame(options) {
 
 		this.output('-------------------');
 		// Reset player, mob, round count
-		this.player = new Hero(heroes.classes[0]);
-		this.mob = new Hero(cavemobs.mobs[3]);
+		this.player = new Hero(heroes.classes[this.playerSelectElem.selectedIndex]);
+		this.mob = new Hero(cavemobs.mobs[this.mobSelectElem.selectedIndex]);
 		this.round = 0;
 
 		// announce the fight & update UI
@@ -139,10 +147,55 @@ function DungeonGame(options) {
 	this.clearLog = function () {
 		this.fightLog.textContent = '';
 	};
+
+	this.init = function () {
+		var i, optgroupElem, optionElem;
+
+		// Populate player <select> with all lvl 1 classes
+		// Create an <optgroup> to hold level 1 hero classes
+		optgroupElem = document.createElement('optgroup');
+		optgroupElem.setAttribute("label", "Level 1");
+
+		// Add each class as an <option>
+		for (i = 0; i < heroes.classes.length; i++) {
+			optionElem = document.createElement('option');
+			optionElem.textContent = heroes.classes[i].name;
+			optionElem.value = i;
+
+			// Add player class <option> to lvl 1 <optgroup>
+			optgroupElem.appendChild(optionElem);
+		}
+
+		// Add lvl 1 <optgroup> to player <select>
+		this.playerSelectElem.appendChild(optgroupElem);
+
+		// Populate mob select box with all cave mobs
+		// Create an <optgroup> to hold all cave mobs
+		optgroupElem = document.createElement('optgroup');
+		optgroupElem.setAttribute("label", "Cave Mobs");
+
+		// Add each cave mob as an <option>
+		for (i = 0; i < cavemobs.mobs.length; i++) {
+			optionElem = document.createElement('option');
+			optionElem.textContent = cavemobs.mobs[i].name;
+			optionElem.value = i;
+
+			// Add mob <option> to cave mob <optgroup>
+			optgroupElem.appendChild(optionElem);
+		}
+
+		// Add cave mob <optgroup> to mob <select>
+		this.mobSelectElem.appendChild(optgroupElem);
+	};
+
+	this.mobSelected = function () {
+		var i, selectedMob;
+	};
 };
 
 window.onload = function () {
 	window.dungeon = new DungeonGame();
 
+	window.dungeon.init();
 	window.dungeon.reset();
 };
