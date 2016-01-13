defc84e203ab7ecf6d80009b8b33ad84ced558d7
Jim Toth <jim@spooky.email>
2016-01-13 02:06:11 -0500

    Fight on the htmls!

--- game.js
+++ game.js
@@ -1,32 +1,58 @@
-function hero(name, hp, atk) {
-	this.heroName = name;
+function Hero(name, hp, atk) {
+	this.name = name;
 	this.hp = hp;
 	this.atk = atk;
-}
+};
 
 function dieRoll(rolls, sides){
-	for(i = 0; i < rolls; i++) {
+	for (i = 0; i < rolls; i++) {
 		return ((Math.random() * sides) + 1).toFixed(0);
 	}
-}
+};
 
-var p1 = new hero("Buttz", 12, 5);
-var mob = new hero("Skele-Bro", 6, 2);
-var count = 0;
+function fight() {
+	var fighterA = new Hero("Buttz", 12, 5);
+	var fighterB = new Hero("Skele-Bro", 6, 2);
 
-while (p1.hp !== 0 && mob.hp !== 0) {
-	//count++;
-	//console.log(count);
-	console.log(mob.hp -= dieRoll(1, p1.atk));
-	console.log("mob");
-	console.log(p1.hp -= dieRoll(1, mob.atk));
-	console.log("player");
+	var count = 0;
 
-	if (p1.hp <= 0) {
-		console.log(mob.heroName + " wins!");
-		break;
-	} else if (mob.hp <= 0) {
-		console.log(p1.heroName + " wins!");
-		break;
+	while (fighterA.hp > 0 && fighterB.hp > 0) {
+		// iterate the roundo
+		count++;
+
+		// fighterA attacks fighterB
+		fighterB.hp -= dieRoll(1, fighterA.atk);
+		output(fighterB.name + ': ' + fighterB.hp);
+		
+		// fighterB attacks fighterA
+		fighterA.hp -= dieRoll(1, fighterB.atk);
+		output(fighterA.name + ': ' + fighterA.hp);
+
+		// evaluate resultzors
+		if (fighterA.hp <= 0) {
+			output(fighterB.name + " wins!");
+		} else if (fighterB.hp <= 0) {
+			output(fighterA.name + " wins!");
+		}
 	}
-}
+};
+
+function output(message) {
+	var messageDiv, fightLog, lineBreak;
+
+	// log to console
+	console.log(message);
+
+	// grab fight-log div
+	fightLog = document.getElementById('fight-log');
+
+	// create a new text node with the message
+	messageText = document.createTextNode(message);
+
+	// create a new <br> element to act as a line break
+	lineBreak = document.createElement('br');
+
+	// append message text and line break
+	fightLog.appendChild(messageText);
+	fightLog.appendChild(lineBreak);
+};
