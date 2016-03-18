4fd7bdceaab572f6f51a2b9adf1ea9b3d70f1ab4
Robert Toth <Robert Toth>
2016-03-17 22:47:18 -0400

    Formatted data files to look more pretty and added source files for tween, preload and sound js

--- data/actions.js
+++ data/actions.js
@@ -1,72 +1,12 @@
 var actions = {
-	'Melee': {
-		'stat': 'atk',
-		'rolls': 1,
-		'target-stat': 'hp',
-		'verb': 'hits for',
-		'verb-damage': 'damage'
-	},
-	'Steal': {
-		'stat': 'atk',
-		'rolls': 1,
-		'target-stat': 'gold',
-		'verb': 'snags',
-		'verb-damage': 'gold'
-	},
-	'Dual Strike': {
-		'stat': 'atk',
-		'rolls': 2,
-		'target-stat': 'hp',
-		'verb': 'bops for',
-		'verb-damage': 'damage'
-	},
-	'Blade Storm': {
-		'stat': 'atk',
-		'rolls': 3,
-		'target-stat': 'hp',
-		'verb': 'SMACKS FOR',
-		'verb-damage': 'hp',
-	},
-	'Wind Strike': {
-		'stat': 'atk',
-		'rolls': 1,
-		'target-stat': 'hp',
-		'verb': 'blows for',
-		'verb-damage': 'wind damage'
-	},
-	'Flame': {
-		'stat': 'atk',
-		'rolls': 1,
-		'target-stat': 'hp',
-		'verb': 'burns for',
-		'verb-damage': 'fire damage'
-	},
-	'Aqua Blast': {
-		'stat': 'atk',
-		'rolls': 1,
-		'target-stat': 'hp',
-		'verb': 'soaks for',
-		'verb-damage': 'water damage'
-	},
-	'Lightning': {
-		'stat': 'atk',
-		'rolls': 1,
-		'target-stat': 'hp',
-		'verb': 'zaps for',
-		'verb-damage': 'lightning damage'
-	},
-	'Double Cast': {
-		'stat': 'atk',
-		'rolls': 2,
-		'target-stat': 'hp',
-		'verb': 'hits for',
-		'verb-damage': 'mixed damage'
-	},
-	'Chaos Storm': {
-		'stat': 'atk',
-		'rolls': 4,
-		'target-stat': 'hp',
-		'verb': 'kaBOOMS for',
-		'verb-damage': 'mixed elemental damage'
-	}
+	'Melee': {			'stat': 'atk',	'rolls': 1,	'target-stat': 'hp',	'verb': 'hits for',		'verb-damage': 'damage',				},
+	'Steal': {			'stat': 'atk',	'rolls': 1,	'target-stat': 'gold',	'verb': 'snags',		'verb-damage': 'gold',					},
+	'Dual Strike': {	'stat': 'atk',	'rolls': 2,	'target-stat': 'hp',	'verb': 'bops for',		'verb-damage': 'damage',				},
+	'Blade Storm': {	'stat': 'atk',	'rolls': 3,	'target-stat': 'hp',	'verb': 'SMACKS FOR',	'verb-damage': 'hp',					},
+	'Wind Strike': {	'stat': 'atk',	'rolls': 1,	'target-stat': 'hp',	'verb': 'blows for',	'verb-damage': 'wind damage',			},
+	'Flame': {			'stat': 'atk',	'rolls': 1,	'target-stat': 'hp',	'verb': 'burns for',	'verb-damage': 'fire damage',			},
+	'Aqua Blast': {		'stat': 'atk',	'rolls': 1,	'target-stat': 'hp',	'verb': 'soaks for',	'verb-damage': 'water damage',			},
+	'Lightning': {		'stat': 'atk',	'rolls': 1,	'target-stat': 'hp',	'verb': 'zaps for',		'verb-damage': 'lightning damage',		},
+	'Double Cast': {	'stat': 'atk',	'rolls': 2,	'target-stat': 'hp',	'verb': 'hits for',		'verb-damage': 'mixed damage',			},
+	'Chaos Storm': {	'stat': 'atk',	'rolls': 4,	'target-stat': 'hp',	'verb': 'kaBOOMS for',	'verb-damage': 'mixed elemental damage'	},
 };
--- data/heroes.js
+++ data/heroes.js
@@ -1,46 +1,6 @@
 var heroes = [
-		{
-			name: "THIEF",
-			lvl: 1,
-			xp: 0,
-			hp: 12,
-			atk: 6,
-			miss: 1,
-			abilities: [
-				'Steal'
-			],
-		},
-		{
-			name: "SORCERESS",
-			lvl: 1,
-			xp: 0,
-			hp: 10,
-			atk: 8,
-			miss: 2,
-			abilities: [
-				'Wind Strike',
-			],
-		},
-		{
-			name: "WARRIOR",
-			lvl: 1,
-			xp: 0,
-			hp: 14,
-			atk: 8,
-			miss: 2,
-			abilities: [
-				'Defend',
-			],
-		},
-		{
-			name: "CLERIC",
-			lvl: 1,
-			xp: 0,
-			hp: 10,
-			atk: 6,
-			miss: 2,
-			abilities: [
-				'Heal',
-			],
-		},
+		{	name: "THIEF",		lvl: 1,	xp: 0,	hp: 12,	atk: 6,	miss: 1,	abilities: [	'Steal',		],	},
+		{	name: "SORCERESS",	lvl: 1,	xp: 0,	hp: 10,	atk: 8,	miss: 2,	abilities: [	'Wind Strike',		],	},
+		{	name: "WARRIOR",	lvl: 1,	xp: 0,	hp: 14,	atk: 8,	miss: 2,	abilities: [	'Defend',		],	},
+		{	name: "CLERIC",		lvl: 1,	xp: 0,	hp: 10,	atk: 6,	miss: 2,	abilities: [	'Heal',			],	},
 	];
